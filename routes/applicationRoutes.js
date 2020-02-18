7e8b8b1871390f06736c8797da3a82f5bb998f94
onecheesepizza <52761974+onecheesepizza@users.noreply.github.com>
2020-02-18 02:10:52 -0800

    implement Webpack and Workbox

--- routes/applicationRoutes.js
+++ routes/applicationRoutes.js
@@ -5,11 +5,11 @@ var isAuthenticated = require("../config/middleware/isAuthenticated");
 var application_controller = require('../controllers/applicationController');
 
 router.get('/', isAuthenticated, application_controller.index);
-router.get('/home', isAuthenticated, application_controller.home);
+router.get('/home', application_controller.home);
 router.get('/signin', application_controller.signin);
 router.get('/signup', application_controller.signup);
-router.get('/workout', isAuthenticated, application_controller.workout);
-router.get('/workouts', isAuthenticated, application_controller.workouts);
+router.get('/workout', application_controller.workout);
+router.get('/workouts', application_controller.workouts);
 router.get('/workouts/:id', isAuthenticated, application_controller.workout);
 
 router.get('/error404', application_controller.error404);
